b1b2d645d7ea4035dc1b3e186100e093ffdee49c
TTP03 <130140359+TTP03@users.noreply.github.com>
2023-12-19 14:47:57 +0700

    edit css + book

--- src/routes/home.js
+++ src/routes/home.js
@@ -5,7 +5,7 @@ const homeController = require('../controllers/HomeController');
 // router.use('/', homeController.getAllTour)
 
 router.get('/search', homeController.getTourBySearch);
-router.get('/transport', homeController.getTourByTransport);
 router.get('/country', homeController.getTourByCountry);
+router.get('/transport', homeController.getTourByTransport);
 router.get('/', homeController.getAllTour);
 module.exports = router;
